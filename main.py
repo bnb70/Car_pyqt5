380f5d8d571939a46f1e2604c5ca3ec9e94bb535
bnb70 <bensonw830@gmail.com>
2023-09-13 17:33:29 +0800

    0.1

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.setup_control()
+        self.main_run()
     def setup_control(self):
         self.ui.Star_Button.clicked.connect(self.clicked_start_button)
         self.ui.Stop_Button.clicked.connect(self.clicked_stop_button)
         self.ui.Confirm_Button.clicked.connect(self.clicked_confirm_button)
+    def main_run(self):
+        if self.state == True:
+            print()
     def clicked_start_button(self):
         self.state = True
+        print(self.state)
         print(f"{self.time01}")
         print(f"{self.time02}")
         print(f"{self.time03}")
@@ -20,7 +25,7 @@ class MainWindow(QtWidgets.QMainWindow):
         print(f"{self.time05}")
     def clicked_stop_button(self):
         self.state = False
-        print(f"")
+        print(self.state)
     def clicked_confirm_button(self):
         self.time01 = self.ui.time_input01.value()
         self.time02 = self.ui.time_input02.value()
